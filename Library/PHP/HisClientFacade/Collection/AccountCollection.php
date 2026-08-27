aadb56db309004ff831704c0a86a20a9d1827363
Simon Praetorius <moin@praetorius.me>
2026-08-27 10:42:51 +0200

    [TASK] ACE-277 Slicable collections
    To get more flexibility for data selection during import, collections
    now implement a `slice()` method, which returns the specified slice
    of the collection. This is the basis for an import process that can
    delegate chunks to subprocesses. It also allows easier implementation
    of paginations.

--- Library/PHP/HisClientFacade/Collection/AccountCollection.php
+++ Library/PHP/HisClientFacade/Collection/AccountCollection.php
@@ -62,6 +62,11 @@ final readonly class AccountCollection implements CollectionInterface, ValidityA
         return array_last($this->items);
     }
 
+    public function slice(int $offset, ?int $length = null): self
+    {
+        return self::fromArray(array_slice($this->items, $offset, $length));
+    }
+
     public function onlyValidAt(\DateTimeInterface $at): self
     {
         return self::fromArray(array_values(array_filter(
--- Library/PHP/HisClientFacade/Collection/PersonAttributeCollection.php
+++ Library/PHP/HisClientFacade/Collection/PersonAttributeCollection.php
@@ -61,6 +61,11 @@ final readonly class PersonAttributeCollection implements CollectionInterface
         return array_last($this->items);
     }
 
+    public function slice(int $offset, ?int $length = null): self
+    {
+        return self::fromArray(array_slice($this->items, $offset, $length));
+    }
+
     public static function getItemType(): string
     {
         return PersonAttribute::class;
